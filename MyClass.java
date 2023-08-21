f4b4a3b19d22390f713d9c20c8bc604f16b705cf
Dharshana Alagarsamy <dharshana.a@itp.objectfrontier.com>
2023-08-21 16:22:32 +0530

    Modified Commit

--- MyClass.java
+++ MyClass.java
@@ -1,13 +1,6 @@
 class MyClass {
 
-    void add(int a,intb)
-    {
-        System.out.println(a+b);
-    }
-    void sub(int a,intb)
-    {
-        System.out.println(a-b);
-    }
+
     void mul(int a,int b)
     {
         System.out.println(a*b);
