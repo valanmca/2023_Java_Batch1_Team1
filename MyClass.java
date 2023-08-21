a0f83b1e3a96147acc69051af4f06c88f92304a1
Domnic Dev Kulanthaisamy <domnic.k@itp.objectfrontier.com>
2023-08-21 16:22:32 +0530

    Domnic's First commit

--- MyClass.java
+++ MyClass.java
@@ -8,6 +8,13 @@ class MyClass {
     {
         System.out.println(a-b);
     }
-
+    void mul(int a,int b)                                           //domnic's method
+    {
+        System.out.println(a+" * "+b+" = "+a*b);              
+    }
+    void div(int a,int b)                                            //domnic's method
+    {
+         System.out.println(a+" divided "+b+" = "+a/b);
+    }
 
 }
