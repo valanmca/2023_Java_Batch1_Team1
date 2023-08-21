d3489891dc6b43d56fcc94286bb8e104f2793c13
Dharshana Alagarsamy <142382814+DharshuAlagar23@users.noreply.github.com>
2023-08-21 16:16:34 +0530

    Commit Changes

--- MyClass.java
+++ MyClass.java
@@ -8,6 +8,13 @@ class MyClass {
     {
         System.out.println(a-b);
     }
-
+    void mul(int a,int b)
+    {
+        System.out.println(a*b);
+    }
+    void div(int a,int b)
+    {
+        System.out.println(a/b);
+    }
 
 }
